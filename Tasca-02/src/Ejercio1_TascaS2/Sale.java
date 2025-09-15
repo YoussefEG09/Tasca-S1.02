d453c73938c62baa801b9a8696ef683565e98151
Youssef <youssef.eg09@gmail.com>
2025-09-15 10:57:47 +0200

    feat : Modifying Sale Class

--- Tasca-02/src/Ejercio1_TascaS2/Sale.java
+++ Tasca-02/src/Ejercio1_TascaS2/Sale.java
@@ -8,11 +8,30 @@ public class Sale {
     private double final_price;
 
 
-
-    public Sale (double final_price) {
+    public Sale(double final_price) {
         this.final_price = final_price;
 
         this.list_products = new ArrayList<>();
+    }
+
+    public List<Product> getList_products() {
+        return list_products;
+    }
+
+    public double getFinal_price() {
+        return final_price;
+    }
+
+    public double Calculate_Total() {
+        if (list_products.isEmpty()) {
+            throw new SaleEmptyException("To make a sale, you first need to add products.");
+        }
+        double totalPrice = 0.0;
+        int i = 0;
+        for (Product:list_products) {
+            totalPrice += Product.getPrice();
+
+        }
     }
 
 
