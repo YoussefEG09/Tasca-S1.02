494ee716e301f5faf5a3cf20d964cd73f9f4da31
Youssef <youssef.eg09@gmail.com>
2025-09-16 10:02:43 +0200

    feat : adding exception and finishing methods

--- Tasca-02/src/Nivel2_TascaS2/Entry.java
+++ Tasca-02/src/Nivel2_TascaS2/Entry.java
@@ -18,7 +18,8 @@ public class Entry {
                 value = scanner.nextByte();
                 valid = true;
             } catch (InputMismatchException e) {
-                System.out.println("Format error");
+                System.out.println("Format error.");
+                scanner.nextLine();
             }
         }
         return value;
@@ -34,8 +35,8 @@ public class Entry {
                 value = scanner.nextInt();
                 valid = true;
             } catch (InputMismatchException e) {
-                System.out.println("Format error");
-
+                System.out.println("Format error.");
+                scanner.nextLine();
             }
 
         }
@@ -54,6 +55,7 @@ public class Entry {
                 valid = true;
             } catch (InputMismatchException e) {
                 System.out.println("Format error.");
+                scanner.nextLine();
             }
 
         }
@@ -61,18 +63,62 @@ public class Entry {
     }
 
     public static double scanDouble(String message) {
+        double value = 0;
+        boolean valid = false;
+
+        while (!valid) {
+            try {
+                System.out.println(message + " : ");
+                value = scanner.nextDouble();
+                valid = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Format error.");
+                scanner.nextLine();
+            }
+        }
+        return value;
+    }
+
+    public static char scanChar(String message) throws FormatErrorException {
+        char value = ' ';
+        boolean valid = false;
+
+        while (!valid) {
+            System.out.println((message + " : "));
+            String input = scanner.next();
+
+            if (input.length() != 1) {
+                throw new FormatErrorException();
+            } else {
+                value = input.charAt(0);
+                valid = true;
+            }
+        }
+
+        return value;
 
     }
 
-    public static char scanChar(String message) {
+    public static String scanString(String message) throws FormatErrorException {
 
+        String value = " ";
+        boolean valid = false;
+
+        while (!valid) {
+            System.out.println(message + " : ");
+            String input = scanner.next();
+
+            if (input.isEmpty()) {
+                throw new FormatErrorException();
+            }
+        }
+
+        return value;
     }
 
-    public static String scanString(String message) {
+}
 
-    }
+public static boolean scanBoolean(String message) throws FormatErrorException {
 
-    public static boolean scanBoolean(String message) {
-
-    }
+}
 }
---
+++ Tasca-02/src/Nivel2_TascaS2/FormatErrorException.java
@@ -0,0 +1,7 @@
+package Nivel2_TascaS2;
+
+public class FormatErrorException extends Exception{
+    public FormatErrorException(){
+        super("You used the wrong format.");
+    }
+}
